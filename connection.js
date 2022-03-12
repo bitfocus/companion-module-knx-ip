9c91454a469a8c090361498f731abc65271a5ab3
Peter Körner <pkoerner@seibert-media.net>
2022-03-12 15:52:08 +0100

    feedback

--- connection.js
+++ connection.js
@@ -21,16 +21,24 @@ exitHook(cb => {
 // for nodemon
 exitHook.hookEvent('SIGUSR2', 0);
 
+function formatDpKey(group_addr, data_type) {
+	return group_addr + '-' + data_type;
+}
+
 class KnxIpConnection {
 	isConnecting = false
 	isConnected = false
 	connection = null
+	dataPoints = {}
+	feedbackState = {}
 
-	onStateChanged = null
+	onStateChangedFn = null
+	onFeedbackChangedFn = null;
 
-	constructor(log, onStateChangedFn) {
+	constructor(log, onStateChangedFn, onFeedbackChangedFn) {
 		this.log = log
 		this.onStateChangedFn = onStateChangedFn
+		this.onFeedbackChangedFn = onFeedbackChangedFn;
 	}
 
 	connect(ip) {
@@ -84,6 +92,49 @@ class KnxIpConnection {
 		if (this.onStateChangedFn) {
 			this.onStateChangedFn(this.isConnecting, this.isConnected)
 		}
+	}
+
+	send(config) {
+		const dp = this.getOrCreateDatapoint(config.group_addr, config.data_type)
+		let value = config.value
+		if (config.data_type === 'DPT1') {
+			value = (value === '1' || value.toLowerCase() === 'true')
+		}
+		if (config.data_type === 'DPT5') {
+			value = parseInt(value)
+		}
+
+		console.log('dp.write', JSON.stringify(value))
+		dp.write(value);
+	}
+
+	updateFeedbackListeners(listenerConfig) {
+		listenerConfig.forEach(config => {
+			this.getOrCreateDatapoint(config.group_addr, config.data_type || 'DPT1')
+		})
+	}
+
+	getOrCreateDatapoint(group_addr, data_type) {
+		const key = formatDpKey(group_addr, data_type)
+		if (!(key in this.dataPoints)) {
+			this.log('🔨 new dp', key)
+			const dp = new knx.Datapoint({ga: group_addr, dpt: data_type, autoread: true}, this.connection);
+			dp.on('change', (oldValue, newValue) => {
+				this.log('💡 dp onChange', key, oldValue, '->', newValue, typeof newValue)
+				this.feedbackState[key] = newValue;
+				this.onFeedbackChangedFn()
+			})
+			this.dataPoints[key] = dp
+		}
+
+		return this.dataPoints[key];
+	}
+
+	getFeedbackState(config) {
+		let group_addr = config.group_addr;
+		let data_type = config.data_type || 'DPT1';
+		const key = formatDpKey(group_addr, data_type)
+		return this.feedbackState[key]
 	}
 }
 
