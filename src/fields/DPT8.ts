fce3da2e66332686ad04995eaef3108cb23f49ef
Peter Körner <pkoerner@seibert-media.net>
2022-04-09 13:07:41 +0200

    fix scale function fpr siogned ranges and add tests

---
+++ src/fields/DPT8.ts
@@ -0,0 +1,23 @@
+import {NumberDPT} from './DPT'
+
+// https://www.knx.org/wAssets/docs/downloads/Certification/Interworking-Datapoint-types/03_07_02-Datapoint-Types-v02.02.01-AS.pdf
+// Chapter 3.9
+export const DPT8: NumberDPT = {
+	type: 'number',
+	id: 'DPT8',
+	label: '16-bit signed',
+	numberRange: [-32_768, 32_767],
+	valueFn: (value: number) => Math.round(value),
+	subtypes: [
+		{id: '001', label: 'Counter Pulses', unit: 'pulses'},
+		{id: '002', label: 'Period (ms)', unit: 'milliseconds'},
+		{id: '003', label: 'Period (10ms)', unit: 'centiseconds'},
+		{id: '004', label: 'Period (100ms)', unit: 'deciseconds'},
+		{id: '005', label: 'Period (s)', unit: 'seconds'},
+		{id: '006', label: 'Period (min)', unit: 'minutes'},
+		{id: '007', label: 'Period (hrs)', unit: 'hours'},
+		{id: '010', label: 'Percent V19', unit: '-327,68%-327,67%', projectedRange: [-327.68, 327.67], step: 0.01},
+		{id: '011', label: 'Angle', unit: '°'},
+		{id: '012', label: 'Altitude above Sea Level', unit: 'm'},
+	]
+}
---
+++ src/fields/ScaleFunction.spec.ts
@@ -0,0 +1,26 @@
+import {scale} from './ScaleFunction'
+
+test('unsigned', () => {
+	expect(scale(0, [0, 255], [0, 100])).toBe(0);
+	expect(scale(10, [0, 255], [0, 100])).toBe(26);
+	expect(scale(25, [0, 255], [0, 100])).toBe(64);
+	expect(scale(50, [0, 255], [0, 100])).toBe(128);
+	expect(scale(75, [0, 255], [0, 100])).toBe(191);
+	expect(scale(100, [0, 255], [0, 100])).toBe(255);
+});
+
+test('signed', () => {
+	expect(scale(-50, [-128, 127], [-50, 50])).toBe(-128);
+	expect(scale(-25, [-128, 127], [-50, 50])).toBe(-64);
+	expect(scale(0, [-128, 127], [-50, 50])).toBe(-0);
+	expect(scale(25, [-128, 127], [-50, 50])).toBe(63);
+	expect(scale(50, [-128, 127], [-50, 50])).toBe(127);
+});
+
+test('unsigned-to-signed', () => {
+	expect(scale(0, [-128, 127], [0, 100])).toBe(-128);
+	expect(scale(25, [-128, 127], [0, 100])).toBe(-64);
+	expect(scale(50, [-128, 127], [0, 100])).toBe(-0);
+	expect(scale(75, [-128, 127], [0, 100])).toBe(63);
+	expect(scale(100, [-128, 127], [0, 100])).toBe(127);
+});
