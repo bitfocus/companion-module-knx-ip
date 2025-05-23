6653db6eeab2d8a8b86d5cd8b1771674f5c475a2
Joseph Adams <josephdadams@gmail.com>
2025-05-23 14:42:44 -0400

    add info field

--- src/Config.ts
+++ src/Config.ts
@@ -2,9 +2,16 @@ import { SomeCompanionConfigField } from '@companion-module/base'
 
 export const CONFIG_FIELDS: SomeCompanionConfigField[] = [
 	{
+		type: 'static-text',
+		id: 'info',
+		label: 'Information',
+		width: 12,
+		value: 'This module connects to a KNX IP Gateway or Router',
+	},
+	{
 		type: 'textinput',
 		id: 'ipAddr',
-		label: 'IP-Address of Gateway or Router',
+		label: 'IP Address of Gateway or Router',
 
 		width: 12,
 	},
